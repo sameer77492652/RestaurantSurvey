d9fa6d7e703d753a6def18f519487ebc4d4445b9
sameer77492652 <Sameersharma078626@gmail.com>
2019-01-25 10:25:23 +0530

    added hide language view

--- app/src/main/java/actiknow/com/restaurantsurvey/utils/AppConfigURL.java
+++ app/src/main/java/actiknow/com/restaurantsurvey/utils/AppConfigURL.java
@@ -6,7 +6,6 @@ public class AppConfigURL {
     public static String URL_INIT = BASE_URL + "v1/init";
     public static String URL_SUBMIT_RESPONSE = BASE_URL + "v1/submit_response";
     public static String URL_LOGIN = BASE_URL + "v1/login";
-    public static String URL_SIGN_IN = BASE_URL + "v1/login";
     public static String URL_FORGOT_PASSWORD = BASE_URL + "v1/forgotPassword";
     public static String URL_UPDATE_PROFILE = BASE_URL + "v1/update";
     public static String URL_UPDATE_POINTS = BASE_URL + "v1/updatePoints";
